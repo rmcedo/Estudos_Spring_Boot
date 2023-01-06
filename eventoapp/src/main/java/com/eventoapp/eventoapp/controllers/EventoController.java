56c91422c3aa54903425a9e886b818cbceb592f1
rmcedo <rafael9408@gmail.com>
2023-01-06 15:28:02 -0300

    Criação da casca do Projeto | Instalação do Materialize CSS Formulário de Eventos | Listagem de Eventos Criação de Banco de Dados no MySQL
    Signed-off-by: rmcedo <rafael9408@gmail.com>

---
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
@@ -0,0 +1,42 @@
+package com.eventoapp.eventoapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventoapp.eventoapp.models.Evento;
+import com.eventoapp.eventoapp.repository.EventoRepository;
+
+@Controller
+public class EventoController {
+
+	@Autowired
+	private EventoRepository er;
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
+	public String form() {
+
+		return "evento/formEvento";
+	}
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+	public String form(Evento evento) {
+
+		er.save(evento);
+
+		return "evento/formEvento";
+	}
+
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+
+	}
+
+}
